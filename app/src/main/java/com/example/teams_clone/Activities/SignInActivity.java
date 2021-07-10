ad34a90f780f7f9876ba526670a3463e090cee85
Akansha Khedia <khedia.akansha25@gmail.com>
2021-07-11 00:15:12 +0530

    Changed the Google sign-in button layout

--- app/src/main/java/com/example/teams_clone/Activities/SignInActivity.java
+++ app/src/main/java/com/example/teams_clone/Activities/SignInActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
@@ -18,7 +19,6 @@ import com.example.teams_clone.utilities.PreferenceManager;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.button.MaterialButton;
@@ -62,8 +62,7 @@ public class SignInActivity extends AppCompatActivity {
                 .requestProfile()
                 .build();
 
-        SignInButton signInButton = findViewById(R.id.sign_in_button);
-        signInButton.setSize(SignInButton.SIZE_STANDARD);
+        Button signInButton = findViewById(R.id.sign_in_button);
 
         signInButton.setOnClickListener(v -> AuthenticationManager.signInG(this));
 
